fe9d79c9888053b36a6ff0b4942ca6b3fc526709
Maruf Mobin <mobinh264@gmail.com>
2022-01-14 23:17:35 +0600

    Cheking purpuss

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from '../../Components/Home/HeroSection';
+
+const Home = () => {
+    return (
+        <div>
+            <HeroSection></HeroSection>
+        </div>
+    );
+};
+
+export default Home;
