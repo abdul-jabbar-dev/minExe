598eaeee29cf4e4ec9798642a712faf96ca57c04
abduljabbar15 <abduljabber1532002@gmail.com>
2022-01-15 22:00:38 +0600

    asdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-<<<<<<< HEAD
 import Footer from './Page/Shared/Footer';
-=======
-
->>>>>>> 4ac97f29328009e77d0d99cb9445475af48b78ee
 import Navigation from './Page/Shared/Navigation';
 import CstomerSatisfaction from './Components/Home/CstomerSatisfaction';
 
@@ -11,26 +7,15 @@ import CstomerSatisfaction from './Components/Home/CstomerSatisfaction';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       {/* NavBar   */}
       <Navigation></Navigation>
-
-
-
-
-
-
-
+      
+     <CstomerSatisfaction></CstomerSatisfaction>
 
 
 
       {/* footer  */}
       <Footer></Footer>
-=======
-     <Navigation></Navigation>
-     <CstomerSatisfaction></CstomerSatisfaction>
-
->>>>>>> 4ac97f29328009e77d0d99cb9445475af48b78ee
     </div>
   );
 }
