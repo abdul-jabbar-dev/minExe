f022bbdefd5537a6fde4799c50bc3add3f2c233e
abduljabbar15 <abduljabber1532002@gmail.com>
2022-01-24 19:41:40 +0600

    add top anilisis

--- src/Api/Chart.js
+++ src/Api/Chart.js
@@ -1,6 +1,6 @@
-const datas = [12, 19, 3, 5, 2]
+const datas = [12, 19, 3, 5]
 const data = {
-    labels: ['Java', 'JS', 'React', 'HTML', 'PHP'],
+    labels: ['Java','React', 'HTML', 'PHP'],
     datasets: [
         {
             label: '# of Votes',
@@ -15,7 +15,6 @@ const data = {
                 '#45C881',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(153, 102, 255, 1)',
             ],
 
 
@@ -28,18 +27,18 @@ const data = {
 
 
 const lineData = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
 
     datasets: [
         {
             label: "First dataset",
-            data: [33, 53, 85, 41, 44, 65],
+            data: [3, 13, 12, 16, 19, 20, 21, 26, 27],
             fill: true,
             animations: {
                 tension: {
                     duration: 10000,
                     easing: 'linear',
-                    from: .4,
+                    from: .5,
                     to: .2,
                     loop: true
                 }
@@ -49,7 +48,7 @@ const lineData = {
         },
         {
             label: "Second dataset",
-            data: [33, 25, 35, 51, 54, 26],
+            data: [4, 10, 10, 14, 15, 18, 20, 24, 30],
             fill: true,
             animations: {
                 tension: {
